91387c0e57cd8c048da18eb69436f80d001c72c2
Daniil Trofimov <trofimovdanil946@gmail.com>
2021-11-07 23:07:03 +0300

    intial commit

--- src/components/atoms/DeleteIcon/index.tsx
+++
@@ -1,8 +0,0 @@
-// import React from 'react';
-// import LogoIcon from '@/public/icons/edit.svg';
-
-// const DeleteIcon = () => {
-//     return <LogoIcon />
-// };
-
-// export default DeleteIcon;
